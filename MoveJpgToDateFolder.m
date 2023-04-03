564c3d63bf80603bce87f76d4c67b928f12fd15e
WangYu <wangyu_1988@126.com>
2023-04-04 00:51:01 +0800

    避免重复文件被覆盖

--- MoveJpgToDateFolder.m
+++ MoveJpgToDateFolder.m
@@ -5,8 +5,8 @@ PhotoFilePatten=('^(IMG|VID)_(?<YMD>\d{8})_(?<HMS>\d{6})');
 
 input_dir = uigetdir('.', 'Pick Photo Directory');
 % input_dir ='C:\Users\wangy\Pictures\PS';
-output_dir = uigetdir('.', 'Pick Output Directory');
-% output_dir ='Z:\3 所有照片\照片按时间放入文件夹';
+% output_dir = uigetdir('.', 'Pick Output Directory');
+output_dir ='D:\3 所有照片\按时间整理';
 cd(input_dir);
 AllObject=dir();
 AllFile=AllObject([AllObject.isdir]==0);
@@ -21,13 +21,17 @@ for iFile=1:length(AllFile)
             if ~exist(fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD),'dir')
                 mkdir(fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD));
             end
-            movefile(fullfile(input_dir,[name ext]),fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD));
-        else
+            if ~exist(fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD,[name ext]),'file')
+                movefile(fullfile(input_dir,[name ext]),fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD));
+            else
+                warning('Same Name File Exist! %s',fullfile(output_dir,PhotoInfo.YMD(1:4),PhotoInfo.YMD,[name ext]));
+            end
+
             continue;
         end
     end
 end
-cd('C:\Users\wangy\Documents\MATLAB\MoveJpgToDateFolder');
+cd('D:\3 所有照片\MoveJpgToDateFolder');
 
 
 
